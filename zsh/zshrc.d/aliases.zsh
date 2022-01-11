aae346011732eb99014ee15f9ecd50106e9395cc
Duncan Wraight <dunc@seriouslyfish.com>
2022-01-11 17:34:52 +0000

    Add oaws alias for using Okta with AWS CLI

--- zsh/zshrc.d/aliases.zsh
+++ zsh/zshrc.d/aliases.zsh
@@ -3,6 +3,29 @@ alias lg="lazygit"
 alias whatismyip="curl ifconfig.me"
 alias gbranch="ZSH_PLUGINS_ALIAS_TIPS_EXCLUDES="git" git rev-parse --abbrev-ref HEAD 2> /dev/null | sed 's/\r//'"
 
+oaws() {
+    # Check to see if the first argument exists as a profile in our Okta configuration
+    grep "\[$1\]" ~/.okta-aws > /dev/null 2&>1
+    if [ $? -ne 0 ]; then
+        profile="importio"
+    else
+        profile=$1
+        shift
+    fi
+
+    oktacmd="okta-awscli --okta-profile $profile --profile $profile"
+
+    # Check to see if the profile has a default region
+    grep -A1 $profile ~/.aws/config | grep region  > /dev/null 2&>1
+    if [ $? -ne 0 ]; then
+        echo "${profile} doesn't have a default region. Running 'aws configure'..."
+        eval "$oktacmd configure"
+    fi
+
+    # Run the rest of our arguments as aws CLI commands
+    eval "$oktacmd $@"
+}
+
 killzoom() {
     ps -aux | grep zoom | grep -v grep | awk '{print $2}' | xargs kill -9
 }

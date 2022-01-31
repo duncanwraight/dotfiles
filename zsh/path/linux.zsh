3e6a0a07ec0e8cafb54dfa7154ad4aa1d1a1bddc
Duncan Wraight <dunc@seriouslyfish.com>
2022-01-31 22:53:32 +0000

    Various fixes and improvements for Mac

--- zsh/path/linux.zsh
+++ zsh/path/linux.zsh
@@ -1,10 +1,15 @@
-export GOPATH=$HOME/work
+# Generic
 path+=("$HOME/.local/bin")
-path+=("$HOME/.local/bin/PurpleExplorer")
+
+# Go-related paths
+export GOPATH=$HOME/work
 path+=('/usr/local/go/bin')
-path+=('/usr/local/Discord')
 path+=("$HOME/work")
 path+=("$HOME/work/bin")
 path+=("$GOPATH/bin")
+
+path+=("$HOME/.local/bin/PurpleExplorer")
+path+=('/usr/local/Discord')
 path+=("$HOME/.garden/bin")
+
 export PATH
---
+++ zsh/path/mac.zsh
@@ -0,0 +1,9 @@
+# Generic
+path+=("$HOME/.local/bin")
+
+# Make sure pip-installed packages are accessible on path
+path+=("$HOME/Library/Python/3.8/bin")
+
+path+=("$HOME/.local/bin/PurpleExplorer")
+
+export PATH

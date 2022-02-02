c2591a5c817689a53732d87f3d88b80f836a31c0
Duncan Wraight <dunc@seriouslyfish.com>
2022-02-02 10:24:36 +0000

    Updates to SSH keys and sshkeys function

--- zsh/zshrc.d/ssh.sh
+++ zsh/zshrc.d/ssh.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-sshkeys () {
-    source $COL_SCRIPT_PATH
+sshkeys() {
+    source $COL_SCRIPT_PATH > /dev/null 2>&1
 
     echo -e "${COL_LBLUE}Removing existing keys...${COL_BLANK}"
     ssh-add -D
 
-    if [[ "$1" == "qp" ]] || [[ "$1" == "zip" ]]; then
-        echo -e "${COL_LBLUE}Adding ${COL_RED}Zip/QuadPay${COL_LBLUE} SSH key${COL_BLANK}"
-        ssh-add $HOME/.ssh/zip/qp_id_ed25519 > /dev/null 2>&1
-    elif [[ "$1" == "import" ]]; then
-        echo -e "${COL_LBLUE}Adding ${COL_RED}import.io${COL_LBLUE} SSH key${COL_BLANK}"
-        ssh-add $HOME/.ssh/import/import_id_ed25519 > /dev/null 2>&1
+    if [ "$#" -ne 1 ]; then
+        echo -e "${COL_RED}Error: sshkeys command accepts only 1 argument${COL_BLANK}"
+        return 1
+    fi
+
+    FOLDER=$1
+
+    if [ ! -d ~/.ssh/${FOLDER} ]; then
+        echo -e "${COL_RED}Error: Folder ${FOLDER} does not exist in ~/.ssh${COL_BLANK}"
+        return 1
     else
-        echo -e "${COL_LBLUE}Adding ${COL_RED}personal${COL_LBLUE} SSH key${COL_BLANK}"
-        ssh-add $HOME/.ssh/personal/pers_id_ed25519 > /dev/null 2>&1
+        echo -e "${COL_LBLUE}Adding ${COL_RED}${FOLDER}${COL_LBLUE} SSH key(s)${COL_BLANK}"
+        for key in $(find ~/.ssh/$FOLDER -type f -follow -not -name "*.pub" -not -name "config" -not -name "known_hosts*") ; do
+            echo "... ${COL_LBLUE}${key}${COL_BLANK}"
+            ssh-add $key > /dev/null 2>&1
+        done
     fi
 }
